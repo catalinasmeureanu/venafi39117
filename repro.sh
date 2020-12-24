d352d63d46b24a3df67359efcb2171fbd3e7ee43
Catalina <51541330+catalinasmeureanu@users.noreply.github.com>
2020-12-24 13:35:52 +0100

    Create repro.sh

---
+++ repro.sh
@@ -0,0 +1,35 @@
+export VAULT_ADDR='http://127.0.0.1:8200'
+
+vault operator init -recovery-shares=1 -recovery-threshold=1 > key.txt
+
+sleep 10
+
+RootToken=`grep 'Initial Root Token:' key.txt | awk '{print $NF}'`
+
+echo "$RootToken"
+
+vault status
+
+vault login "$RootToken"
+
+SHA256=$(sha256sum /Users/catalina/Documents/Tickets/39117/etc/vault/plugins/venafi-pki-monitor |cut -d' ' -f1)
+
+vault policy write admin admin.hcl
+
+vault policy list
+
+vault token create -policy="admin" \
+    -format=json | jq -r ".auth.client_token" > token.txt
+
+vault login $(cat token.txt)
+
+# vault write sys/plugins/catalog/secret/venafi-pki-monitor \
+  #  sha_256="${SHA256}" command="venafi-pki-monitor" 
+
+vault plugin register -sha256=$SHA256 venafi-pki-monitor
+
+vault secrets enable -path=pki -plugin-name=venafi-pki-monitor plugin
+
+vault secrets disable venafi-pki-monitor
+
+vault secrets list 
